2525521c6944a898a57166fe703bed7cbcd6d0e0
Robson Lima <robsonlima@Robsons-MacBook-Pro.local>
2021-04-16 03:15:02 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, json
+from alpha_vantage.timeseries import TimeSeries
+
+app = Flask(__name__)
+API_key = 'SBAHO6UH7BX7GIBV'
+
+@app.route('/symbols/<symbol>', methods=['GET'])
+def symbols(symbol):
+    ts = TimeSeries(key=API_key)
+    data, meta_data = ts.get_symbol_search(symbol)
+    return data.to_json(orient='records')[1:-1].replace('},{', '} {')
+
+@app.route('/quotes/<symbol>', methods=['GET'])
+def quotes(symbol):
+    ts = TimeSeries(key=API_key)
+    data, meta_data = ts.get_quote_endpoint(symbol)
+    return json.dumps(data, indent=4)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=105)
