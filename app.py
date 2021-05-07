dd2f97f7354c4a077e2837213df42389ea79d3ef
Escritorio <Escritorio@MACBOOK>
2021-05-06 23:49:27 -0300

    stocks

--- app.py
+++ app.py
@@ -5,25 +5,9 @@ from flask import Flask, request, json, jsonify
 import yfinance as yf
 from pymongo import MongoClient
 
-
 app = Flask(__name__)
 client = MongoClient("mongodb+srv://app:Rkl2021@cluster0.pcxk9.mongodb.net/bovespa?retryWrites=true&w=majority")
 
-
-@app.route('/login/<email>/<password>', methods=['GET'])
-def login(email, password):
-    try:
-        db = client['bovespa']
-        collection = db['users']
-        cursor = collection.find({'email': email, 'password': hashlib.md5(password.encode("utf-8")).hexdigest()},
-                                 {'_id': False, 'password': False})
-
-        for document in cursor:
-            return json.loads(json_util.dumps(document))
-
-        return {}
-    except Exception as ex:
-        return ex
 
 @app.route('/stocks', methods=['GET'])
 def getStocks():
@@ -33,39 +17,28 @@ def getStocks():
 
     stocks = []
     for i, stock in enumerate(cursor):
-        #if i == 1:
+        # if i == 1:
         msft = yf.Ticker(stock['name'] + ".SA")
         stock['info'] = msft.info
         stocks.append(stock)
 
     return jsonify(stocks)
 
-@app.route('/users/<email>', methods=['GET'])
-def getUser(email):
-    try:
-        db = client['bovespa']
-        collection = db['users']
-        cursor = collection.find({'email': email}, {'_id': False, 'password': False})
-
-        for i, document in enumerate(cursor):
-            for i, stock in enumerate(document['stocks']):
-                msft = yf.Ticker(document['stocks'][i]['name'] + ".SA")
-                document['stocks'][i]['info'] = msft.info
-                #document['wallet'][i]['regularMarketPrice'] = msft.info['regularMarketPrice']
-                #document['wallet'][i]['logo_url'] = msft.info['logo_url']
-
-            return json.loads(json_util.dumps(document))
-        return {}
-    except Exception as ex:
-        return ex
-
-@app.route('/users', methods=['POST'])
-def putUser():
+@app.route('/stocks', methods=['POST'])
+def postStock():
     db = client['bovespa']
-    collection = db['users']
-    user = request.json
-    collection.replace_one({"email": user['email']}, user, upsert=True)
-    return user
+    collection = db['stocks']
+    stock = request.json
+    collection.replace_one({"name": stock['name']}, stock, upsert=True)
+    return stock
+
+@app.route("/stocks/<name>", methods=["DELETE"])
+def stockDelete(name):
+    db = client['bovespa']
+    collection = db['stocks']
+    collection.find_one_and_delete({'name': name})
+
+    return jsonify({'name': name})
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,18 +1,27 @@
 import ssl
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import yfinance as yf
 import hashlib
 
 client = MongoClient("mongodb+srv://app:Rkl2021@cluster0.pcxk9.mongodb.net/bovespa?retryWrites=true&w=majority",
                      ssl=True, ssl_cert_reqs=ssl.CERT_NONE)
 
 db = client['bovespa']
-collection = db['users']
+collection = db['stocks']
 
 #INSERT
 #collection.insert_one({'email': 'teste@teste.com', 'password': hashlib.md5(b'bovespa2020').digest(),
 #                       'role': 'CLIENT',
 #                       'wallet': [{'name': 'VALE3'}, {'name': 'AZUL4'}]})
+
+db = client['bovespa']
+collection = db['stocks']
+cursor = collection.find({}, {'_id': False})
+
+for i, stock in enumerate([{'name': 'CSNA3'}, {'name': 'WEGE3'}, {'name': 'PRIO3'}, {'name': 'MGLU3'}, {'name': 'BRAP4'}, {'name': 'VALE3'}, {'name': 'USIM5'}, {'name': 'B3SA3'}, {'name': 'SUZB3'}, {'name': 'RENT3'}]):
+    collection.insert_one(stock)
+
 
 #SELECT
 #cursor = collection.find({'email': 'robsonklima@gmail.com', 'password': hashlib.md5("bovespa".encode("utf-8")).hexdigest()}, {'_id': False})
@@ -24,11 +33,11 @@ collection = db['users']
 #      print(document)
 
 #UPDATE
-collection.update_one({'email': 'robsonklima@gmail.com'},
-                      {"$set": {'password': hashlib.md5("bovespa".encode("utf-8")).hexdigest(),
-                                'role': 'ADMIN', 'name': 'Robson Lima',
-                                'wallet': [{'name': 'ABEV'}, {'name': 'VALE3'},
-                                           {'name': 'BBDC3'}, {'name': 'BBAS3'}]}})
+#collection.update_one({'email': 'robsonklima@gmail.com'},
+#                      {"$set": {'password': hashlib.md5("bovespa".encode("utf-8")).hexdigest(),
+#                                'role': 'ADMIN', 'name': 'Robson Lima',
+#                                'wallet': [{'name': 'ABEV'}, {'name': 'VALE3'},
+#                                           {'name': 'BBDC3'}, {'name': 'BBAS3'}]}})
 
 #DELETE
 #collection.delete_many({'email':'teste@teste.com'})
